a99cb61cb7c843cbcb20272d6598f6c911812122
Sasanka <sajewsasanka1@gmail.com>
2023-07-09 01:03:54 +0530

    Create Test Cases

---
+++ test_model.py
@@ -0,0 +1,16 @@
+import pytest
+import torch
+from model import NeuralNet
+
+
+def test_NeuralNet_forward():
+    input_size = 10
+    hidden_size = 8
+    output_size = 5
+    batch_size = 16
+
+    model = NeuralNet(input_size, hidden_size, output_size)
+    input_data = torch.randn(batch_size, input_size)
+    output = model(input_data)
+
+    assert output.shape == torch.Size([batch_size, output_size])
---
+++ test_nltk_utils.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+from nltk_utils import tokenize, stem, bag_of_words
+
+
+def test_tokenize():
+    sentence = "Hello, how are you?"
+    expected_tokens = ["Hello", ",", "how", "are", "you", "?"]
+    assert tokenize(sentence) == expected_tokens
+
+
+def test_stem():
+    word = "running"
+    expected_stemmed_word = "run"
+    assert stem(word) == expected_stemmed_word
+
+
+def test_bag_of_words():
+    tokenized_sentence = ["Hello", "world", "!"]
+    all_words = ["hello", "world", "goodbye"]
+    expected_bag = np.array([1, 1, 0], dtype=np.float32)
+    assert np.array_equal(bag_of_words(tokenized_sentence, all_words), expected_bag)
